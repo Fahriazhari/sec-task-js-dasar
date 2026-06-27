508c5e6133690a3b6e7026a52f95e8586e01eed6
Fahriazhari <fahrimaulanaazhari392@gmail.com>
2026-06-27 21:51:04 +0700

    Melengkapi

--- loopingAktivitas.js
+++ loopingAktivitas.js
@@ -1,11 +1,11 @@
 for (let angka = 1; angka <= 20; angka++) {
-    if (angka % 2 !== 0 && angka % 3 !== 0) {
-        console.log(`${angka} Apel Pagi`);
-    } else if (angka % 2 === 0 && angka % 3 !== 0) {
-        console.log(`${angka} Mengikuti Pelatihan`);
-    } else if (angka % 2 !== 0 && angka % 3 === 0) {
-        console.log(`${angka} Mengikuti Uji Kompetisi`);
-    } else {
-        console.log(`${angka} Mendapatkan Sertifikat`);
+    if (angka % 2 !== 0 && angka % 3 !== 0) { // Jika angka tidak habis dibagi 2 dan tidak habis dibagi 3
+        console.log(`${angka} - Apel Pagi`); // Maka print angka dan aktivitas apel pagi
+    } else if (angka % 2 === 0 && angka % 3 !== 0) { // Jika angka habis dibagi 2 dan tidak habis dibagi 3
+        console.log(`${angka} - Mengikuti Pelatihan`); // Maka print angka dan aktivitas mengikuti pelatihan
+    } else if (angka % 2 !== 0 && angka % 3 === 0) { // Jika angka tidak habis dibagi 2 dan habis dibagi 3
+        console.log(`${angka} - Mengikuti Uji Kompetisi`); // Maka print angka dan aktivitas mengikuti uji kompetisi
+    } else { // Jika angka habis dibagi 2 dan habis dibagi 3
+        console.log(`${angka} - Mendapatkan Sertifikat`); // Maka print angka dan aktivitas mendapatkan sertifikat
     }
 }
